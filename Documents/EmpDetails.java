4a97f4fd8e78253d144350cea2686eb1f70a442a
AKSHAY <01fe22bca094@kletech.ac.in>
2024-03-19 11:38:40 +0530

    Updated the error

--- Documents/EmpDetails.java
+++ Documents/EmpDetails.java
@@ -1,6 +1,6 @@
-public class EmpDetails extends Person{
+class EmpDetails{
     public int empid = 101;
-    public float salary = 10000.00;
+    public float salary = 10000.00f;
 
     public void display(){
         System.out.println("Empid: " + empid);
